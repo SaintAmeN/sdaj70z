11ea6058733b14a4cd62c0891abff04d1d25a9af
Paweł Recław <pawel.reclaw@gmail.com>
2021-01-24 09:19:55 +0100

    Check

---
+++ src/main/java/pl/sda/zadania/czas/zad2/Main.java
@@ -0,0 +1,9 @@
+package pl.sda.zadania.czas.zad2;
+
+/**
+ * @Author amen
+ * @Created 24.01.2021 11:31
+ * @Project zadania_zdalne_pl_70
+ */
+public class Main {
+}
---
+++ src/main/java/pl/sda/zadania/list/zad1/MainProwadzacy.java
@@ -0,0 +1,12 @@
+package pl.sda.zadania.list.zad1;
+
+/**
+ * @Author amen
+ * @Created 24.01.2021 09:19
+ * @Project zadania_zdalne_pl_70
+ */
+public class MainProwadzacy {
+    public static void main(String[] args) {
+
+    }
+}
