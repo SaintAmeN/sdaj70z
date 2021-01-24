b155a61ade1f21d9416282d35bc9d1454e35b4bd
Paweł Recław <pawel.reclaw@gmail.com>
2021-01-24 13:10:30 +0100

    List zad 5.

---
+++ src/main/java/pl/sda/zadania/list/zad5/MainProwadzacy.java
@@ -0,0 +1,9 @@
+package pl.sda.zadania.list.zad5;
+
+/**
+ * @Author amen
+ * @Created 24.01.2021 13:07
+ * @Project zadania_zdalne_pl_70
+ */
+public class MainProwadzacy {
+}
---
+++ src/main/java/pl/sda/zadania/list/zad5/Płeć.java
@@ -0,0 +1,9 @@
+package pl.sda.zadania.list.zad5;
+
+/**
+ * @Author amen
+ * @Created 24.01.2021 13:05
+ * @Project zadania_zdalne_pl_70
+ */
+public enum Płeć {
+}
