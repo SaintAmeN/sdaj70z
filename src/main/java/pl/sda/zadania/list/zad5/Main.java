799d5d4ac79668021cdc4fc176b21b2dd9f61fc4
Paweł Recław <pawel.reclaw@gmail.com>
2021-01-24 13:02:53 +0100

    List zad 5.

---
+++ src/main/java/pl/sda/zadania/list/zad5/Main.java
@@ -0,0 +1,32 @@
+package pl.sda.zadania.list.zad5;
+
+import java.util.Scanner;
+
+/**
+ * @Author amen
+ * @Created 24.01.2021 13:00
+ * @Project zadania_zdalne_pl_70
+ */
+public class Main {
+    /**
+     * ​Stwórz klasę Student która posiada pola:
+     *  - nr indeksu                (String nrIndeksu)
+     *  - imie                      (String imie)
+     *  - nazwisko                  (String nazwisko)
+     *  - płeć (wartość enum)       (Płeć plec)
+     *
+     *  public static enum Płeć{ MEZCZYZNA, KOBIETA; }
+     *
+     *  W mainie stwórz instancję kolekcji typu ArrayList, która zawiera obiekty klasy Student.
+     *  Dodaj do kolekcji kilku studentów (dodaj je ręcznie - wpisz cokolwiek).
+     *      a. Spróbuj wypisać elementy listy stosując zwykłego "sout'a"
+     *      b. Spróbuj wypisać elementy stosując pętlę foreach
+     *      c. Wypisz tylko kobiety
+     *      d. Wypisz tylko mężczyzne.
+     *
+     *      Wypisz tylko indeksy osób
+     */
+    public static void main(String[] args) {
+
+    }
+}
