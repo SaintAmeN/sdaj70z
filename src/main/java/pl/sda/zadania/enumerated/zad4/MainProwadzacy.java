82a949192cc97e2f2cd681177a52e9fef2f25b83
Paweł Recław <pawel.reclaw@gmail.com>
2021-01-30 11:23:15 +0100

    Zad 4 enum.

---
+++ src/main/java/pl/sda/zadania/enumerated/zad4/MainProwadzacy.java
@@ -0,0 +1,23 @@
+package pl.sda.zadania.enumerated.zad4;
+
+/**
+ * @Author amen
+ * @Created 30.01.2021 11:21
+ * @Project zadania_zdalne_pl_70
+ */
+public class MainProwadzacy {
+    public static void main(String[] args) {
+        /**
+         Stwórz enum TypRoweru i daj mu dwie wartości: ROWER, TANDEM.
+         Dodatkowo enum posiada pole o nazwie ilośćMiejsc. Dla enuma ROWER pole ma wartość 1, dla TANDEM’u ma wartość 2.
+         - Stwórz metodę ‘pobierzIlośćMiejsc():int’.
+
+         - Stwórz klasę Rower która posiada pola:
+            ilośćPrzerzutek, typRoweru, nazwaRoweru
+
+         - Stwórz metodę ‘wypiszInformacjeORowerze():void” która wypisuje informacje oraz podaje/wypisuje ilość miejsc (w postaci liczby).
+         - Stwórz main’a, w metodzie main stwórz 3 rowery, przechowaj je w liście i wypisz je pętlą for.
+         **/
+
+    }
+}
