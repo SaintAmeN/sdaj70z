aa1f4a777abefb4d98b36e9e9503f1e296ca6186
Paweł Recław <pawel.reclaw@gmail.com>
2021-01-30 13:04:12 +0100

    Exception zad 1 i 2 na zaj.

---
+++ src/main/java/pl/sda/zadania/exceptions/zad1/Main.java
@@ -0,0 +1,18 @@
+package pl.sda.zadania.exceptions.zad1;
+
+/**
+ * @Author amen
+ * @Created 30.01.2021 12:48
+ * @Project zadania_zdalne_pl_70
+ */
+public class Main {
+    /**
+     * Napisz program, który prosi użytkownika o dwie liczby(całkowite) a i b,
+     * i wyświetla wynik dzielenia a/b. Jeżeli b jest ujemne program powinien wyświetlić odpowiedni komunikat.
+     * (Dwie metody [przyjmujemy dwa argumenty])
+     * Wariant 1: Zastosuj instrukcję if            (zamiast exception wyświetlamy komunikat)
+     * Wariant 2: Zastosuj instrukcję try-catch     (wywołujemy metodę i przechwycenie odbywa się w main)
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/pl/sda/zadania/exceptions/zad2/Main.java
@@ -0,0 +1,21 @@
+package pl.sda.zadania.exceptions.zad2;
+
+/**
+ * @Author amen
+ * @Created 30.01.2021 12:48
+ * @Project zadania_zdalne_pl_70
+ */
+public class Main {
+
+    /**
+     * Napisz program, a w nim metodę która pyta użytkownika o dwie liczby a następnie sprawdza czy druga liczba nie jest ==0.
+     * Pytanie i scanner (pobranie danych) zawrzyj w Main, instrukcja ma wykonywać sprawdzenia i rzucenie exception.
+     * Przechwycenie ma nastąpić w main.
+     *
+     * Jeśli jest, to wyrzuca wyjątek ‘CholeroException’ z komunikatem
+     * “Pamiętaj cholero, by nie dzielić przez zero!”.
+     */
+    public static void main(String[] args) {
+
+    }
+}
